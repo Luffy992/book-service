5da7b60a6452b724996fa3519c8494d8f6c76cec
Somesh Ramola <luffydi9890@gmail.com>
2026-06-15 21:12:33 +0530

    Integrated Eureka service discovery with OpenFeign

--- src/main/java/com/som/bookservice/client/UserClient.java
+++ src/main/java/com/som/bookservice/client/UserClient.java
@@ -3,10 +3,7 @@ package com.som.bookservice.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(
-        name = "user-service",
-        url = "http://localhost:8081"
-)
+@FeignClient(name = "user-service")
 public interface UserClient {
 
     @GetMapping("/users/test")
