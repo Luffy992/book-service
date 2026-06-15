8cb0278eb692a90cb11af71ad6c7c141a112ff0c
Somesh Ramola <luffydi9890@gmail.com>
2026-06-15 14:14:57 +0530

    Initial book-service setup

---
+++ src/main/java/com/som/bookservice/entity/Book.java
@@ -0,0 +1,14 @@
+package com.som.bookservice.entity;
+
+public class Book {
+
+    private Long id;
+
+    private String title;
+
+    private String author;
+
+    private Double price;
+
+    private Integer stock;
+}
